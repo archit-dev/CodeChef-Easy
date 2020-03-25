7aa338ad177510e81f9c609382fbf6b9ef078b81
Archit Dev <architdev123@gmail.com>
2020-03-25 15:13:40 +0530

    Life, the Universe, and Everything (TEST)

---
+++ TEST.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int s;
+	while(true){
+	    cin>>s;
+	    if(s==42){
+	        break;
+	    }
+	    else{
+	        cout<<s<<endl;
+	    }
+	}
+	return 0;
+}
